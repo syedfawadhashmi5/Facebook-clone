e05edeebdfafb020147b0409329cf31ba5fc196e
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-08-24 01:40:33 -0700

    work on header
    header is ready  add some classes and script on header

---
+++ build/App.js
@@ -0,0 +1,8 @@
+
+function icon__hide(){
+document.getElementById("hide_icon").innerHTML = "";
+document.getElementById("remove__img").style.display='none';
+var icon__element = document.getElementById("add__icon");
+icon__element.classList.remove("hide");
+
+}
