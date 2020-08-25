0e5676cab05c0586a46b7d10733fc46aa939dc0a
sherazshaikh <shaikhsheraz7@gmail.com>
2020-08-25 05:45:44 -0700

    write js code

--- build/App.js
+++ build/App.js
@@ -4,5 +4,52 @@ document.getElementById("hide_icon").innerHTML = "";
 document.getElementById("remove__img").style.display='none';
 var icon__element = document.getElementById("add__icon");
 icon__element.classList.remove("hide");
+icon__element.style.display="block"
+
+//   on click default input display
+icon__element.addEventListener('click',()=>{
+    icon__element.style.display="none"
+    document.getElementById("hide_icon").innerHTML = "search";
+    document.getElementById("remove__img").style={display:'block'};
+
+});
 
 }
+
+
+
+
+
+         // active class script done !!!!
+
+let commonClassAlln = document.querySelectorAll('.commom-cLass')
+    commonClassAlln.forEach(elem => {
+        elem.addEventListener('click',()=>{
+            document.querySelector('.active').classList.remove('active')
+            elem.classList.add('active')
+        });
+    });
+        
+
+
+let leftSide = document.querySelectorAll('.left-status-content')
+        leftSide.forEach(a =>{
+            a.setAttribute('onmouseover','display(this)')
+            a.setAttribute('onmouseout','displayNone()')
+        });
+      
+       
+let hoverDisplay = document.getElementsByClassName('hover-display')[0]
+    function display(e){
+        setTimeout(()=>{
+            hoverDisplay.style.display="block";
+            let userDetaile = document.getElementById('user-detaile')
+            userDetaile.innerHTML = e.children[1].innerText
+        },300);
+    };
+
+    function displayNone(){
+        setTimeout(()=>{
+            hoverDisplay.style.display="none";
+        },300);
+    };
